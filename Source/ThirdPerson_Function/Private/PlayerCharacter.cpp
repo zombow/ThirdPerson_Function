ad82596120e6aa4e2247354f42e3a1998b9dd0f4
zombow <82148187+zombow@users.noreply.github.com>
2024-10-28 19:02:37 +0900

    [10/28] PlayerMesh 추가
    PlayerMesh추가후 테스트

--- Source/ThirdPerson_Function/Private/PlayerCharacter.cpp
+++ Source/ThirdPerson_Function/Private/PlayerCharacter.cpp
@@ -6,9 +6,9 @@
 // Sets default values
 APlayerCharacter::APlayerCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PlayerMesh = CreateDefaultSubobject<USkeletalMeshComponent>("PlayerMesh");
 	PrimaryActorTick.bCanEverTick = true;
-
+	
 }
 
 // Called when the game starts or when spawned
--- Source/ThirdPerson_Function/Public/PlayerCharacter.h
+++ Source/ThirdPerson_Function/Public/PlayerCharacter.h
@@ -14,11 +14,14 @@ class APlayerCharacter : public ACharacter
 public:
 	// Sets default values for this character's properties
 	APlayerCharacter();
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = true))
+	UMeshComponent* PlayerMesh;
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
+	
+	
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
