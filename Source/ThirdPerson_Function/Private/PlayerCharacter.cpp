87060df273c6f1dbb4640cd52a4f174444851576
zombow <82148187+zombow@users.noreply.github.com>
2024-11-01 21:09:24 +0900

    [11/01] CameraBoom추가 , 마우스 회전 적용 1차 테스트
     CameraBoom마우스 회전 적용 테스트

--- Source/ThirdPerson_Function/Private/PlayerCharacter.cpp
+++ Source/ThirdPerson_Function/Private/PlayerCharacter.cpp
@@ -3,21 +3,40 @@
 
 #include "PlayerCharacter.h"
 
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+
 // Sets default values
 APlayerCharacter::APlayerCharacter()
 {
+	// 카메라붐 설정
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>("CameraBoom");
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->SetUsingAbsoluteRotation(true);
+	CameraBoom->TargetArmLength = 400.f;
+	CameraBoom->SetRelativeLocation(FVector(0.0f,0.0f,60.0f));
+	CameraBoom->SetRelativeRotation(FRotator(-10.f, 0.f, 0.f),false);
+	CameraBoom->bDoCollisionTest = true;
+	CameraBoom->bUsePawnControlRotation = true;
+
+	// 카메라 설정
+	PlayerCameraComp = CreateDefaultSubobject<UCameraComponent>("PlayerCamera");
+	PlayerCameraComp->SetupAttachment(CameraBoom,USpringArmComponent::SocketName);
+	PlayerCameraComp->bUsePawnControlRotation = false;
+
+	// 플레이어 스켈레톤 설정
 	PlayerSkeletalMeshComp = FindComponentByClass<USkeletalMeshComponent>();
-	if (LoadObject<USkeletalMesh>(nullptr,TEXT("/Script/Engine.SkeletalMesh'/Game/Characters/Mannequins/Meshes/SKM_Manny.SKM_Manny'"))) // TODO:load좌표를 다른곳에서 관리
+	if (LoadObject<USkeletalMesh>(
+		nullptr,TEXT("/Script/Engine.SkeletalMesh'/Game/Characters/Mannequins/Meshes/SKM_Manny.SKM_Manny'"))) // TODO:load좌표를 다른곳에서 관리
 	{
-		PlayerSkeletalMeshComp->SetSkeletalMesh(LoadObject<USkeletalMesh>(nullptr,TEXT("/Script/Engine.SkeletalMesh'/Game/Characters/Mannequins/Meshes/SKM_Manny.SKM_Manny'")));
+		PlayerSkeletalMeshComp->SetSkeletalMesh(LoadObject<USkeletalMesh>(
+			nullptr,TEXT("/Script/Engine.SkeletalMesh'/Game/Characters/Mannequins/Meshes/SKM_Manny.SKM_Manny'")));
 	}
 	else
 	{
 		UE_LOG(LogTemp, Warning, TEXT("Can't Find SkeletalMesh..."));
 	}
 	
-	PlayerCameraComp = CreateDefaultSubobject<UCameraComponent>("PlayerCamera");
-	PlayerCameraComp->SetupAttachment(RootComponent);
 	PrimaryActorTick.bCanEverTick = true;
 }
 
@@ -25,6 +44,15 @@ APlayerCharacter::APlayerCharacter()
 void APlayerCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+
+	// Enhanced Input 시스템 설정 (PlayerController에서 Enhanced Input Subsystem 사용
+	if(TObjectPtr<APlayerController> PlayerController = Cast<APlayerController>(Controller))
+	{
+		if(TObjectPtr<UEnhancedInputLocalPlayerSubsystem> Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(IMC_TPSCharacter, 0);
+		}
+	}
 }
 
 // Called every frame
@@ -37,4 +65,22 @@ void APlayerCharacter::Tick(float DeltaTime)
 void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	// Enhanced Input 컴포넌트로 캐스팅
+	TObjectPtr<UEnhancedInputComponent> EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+	if(EnhancedInputComponent)
+	{
+		// Look 액션 바인딩
+		EnhancedInputComponent->BindAction(IA_Look, ETriggerEvent::Triggered, this, &APlayerCharacter::Look);
+	}
+}
+
+// Look 액션 함수 구현
+void APlayerCharacter::Look(const FInputActionValue& Value)
+{
+	FVector2D LookAxisValue = Value.Get<FVector2D>();
+
+	// 마우스의 X와 Y 입력 값을 가져와 카메라 회전에 적용
+	AddControllerYawInput(LookAxisValue.X);
+	AddControllerPitchInput(LookAxisValue.Y);
 }
--- Source/ThirdPerson_Function/Public/PlayerCharacter.h
+++ Source/ThirdPerson_Function/Public/PlayerCharacter.h
@@ -3,8 +3,11 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "InputActionValue.h"
 #include "Camera/CameraComponent.h"
 #include "GameFramework/Character.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "ThirdPerson_Function/ThirdPerson_FunctionCharacter.h"
 #include "PlayerCharacter.generated.h"
 
 UCLASS()
@@ -19,17 +22,23 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = true))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = true), Category = "SkeletalMeshComponent")
 	TObjectPtr<USkeletalMeshComponent> PlayerSkeletalMeshComp;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = true))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = true), Category = "CameraComponent")
 	TObjectPtr<UCameraComponent> PlayerCameraComp;
-	
-	
-public:	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = true), Category = "CameraBoom")
+	TObjectPtr<USpringArmComponent> CameraBoom;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = true), Category = "Input")
+	TObjectPtr<UInputMappingContext> IMC_TPSCharacter;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = true), Category = "Input")
+	TObjectPtr<UInputAction> IA_Look;
+
+	void Look(const FInputActionValue& m_Value);
+
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
 };
