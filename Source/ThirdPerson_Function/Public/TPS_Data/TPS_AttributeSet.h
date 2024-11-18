8880cc0fdde2e636a39e4fffae280118db27cf17
zombow <82148187+zombow@users.noreply.github.com>
2024-11-18 19:52:35 +0900

    [11/18] PlayerState.cpp 파일, AttributeSet.cpp 파일 추가
    GAS를 위한 PlayerState, AttributeSet.cpp 파일을 추가하였습니다.
    
    GAS의 프레임워크에대한 이해를 위해 좀더 공부하고 있습니다.

---
+++ Source/ThirdPerson_Function/Public/TPS_Data/TPS_AttributeSet.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "TPS_AttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THIRDPERSON_FUNCTION_API UTPS_AttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ThirdPerson_Function/Public/TPS_Player/TPS_PlayerState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "TPS_PlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THIRDPERSON_FUNCTION_API ATPS_PlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	
+};
