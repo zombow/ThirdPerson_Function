0115fc9d80adf381efd38356a285e1511ab3c3f1
zombow <82148187+zombow@users.noreply.github.com>
2024-11-19 21:48:33 +0900

    [11/19] GAS관련 파일 위치이동
    AttributeSet / AbilitySet / GameEffect 위치변경
    AssetManaget 위치변경

--- Source/ThirdPerson_Function/Private/TPS_Data/TPS_GamePlayAbilitySystem/TPS_AbilitySet.cpp
+++ Source/ThirdPerson_Function/Private/TPS_Data/TPS_GamePlayAbilitySystem/TPS_AbilitySet.cpp
@@ -1,6 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "TPS_Player/TPS_AssetManager.h"
-
+#include "TPS_Data/TPS_GamePlayAbilitySystem/TPS_AbilitySet.h"
 
--- Source/ThirdPerson_Function/Private/TPS_Data/TPS_GamePlayAbilitySystem/TPS_GameEffect.cpp
+++ Source/ThirdPerson_Function/Private/TPS_Data/TPS_GamePlayAbilitySystem/TPS_GameEffect.cpp
@@ -1,5 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "TPS_Data/TPS_GameEffect.h"
+#include "TPS_Data/TPS_GamePlayAbilitySystem/TPS_GameEffect.h"
 
